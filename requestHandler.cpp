5ef8ce76cf438b54d2c562a442ef440bc5e4b728
Shilo <shiloteam@gmail.com>
2025-10-14 21:48:18 +0300

    added *poor* support for images

--- requestHandler.cpp
+++ requestHandler.cpp
@@ -73,42 +73,63 @@ void requestHandler::generateGetResponse() {
 	//else if(getRoute().substr(0, 7) == "/files/" && argDirectory) {
 	else if(getRoute().substr(0, 7) == "/files/") {
 		setRoute(getRoute().erase(0, 7));
-		std::fstream file {};
-		file.open(getRoute(), std::ios::in);
-		if(file.is_open() && !std::filesystem::is_directory(getRoute())) {
-			std::string line{};
-			m_response <<
- 				"HTTP/1.1 200 OK\r\n"
-  			"Content-Type: text/html\r\n"
-  			"Content-Length: " << std::filesystem::file_size(getRoute()) << "\r\n"
-				//  			"Connection: Close\r\n"
-  			"\r\n"; 
-			while(std::getline(file, line)) {
-  			m_response << line << '\n';	
-  		}
-  		//	 m_response << '\n';
-  		std::cout << "sending code 200\n";
-  	}
-		else {
-		  generate404();
+
+		if(getRoute().substr(0, 7) == "images/") {
+			std::fstream file {};
+			file.open(getRoute(), std::ios::in);
+			if(file.is_open() && !std::filesystem::is_directory(getRoute())) {
+				std::string line{};
+				m_response <<
+ 					"HTTP/1.1 200 OK\r\n"
+  				"Content-Type: image/png\r\n"
+  				"Content-Length: " << std::filesystem::file_size(getRoute()) << "\r\n"
+					//  			"Connection: Close\r\n"
+  				"\r\n"; 
+				while(std::getline(file, line)) {
+  				m_response << line << '\n';	
+  			}
+  			//	 m_response << '\n';
+  			std::cout << "sending code 200\n";
+			}
 		}
-	}
+		else {
+			std::fstream file {};
+			file.open(getRoute(), std::ios::in);
+			if(file.is_open() && !std::filesystem::is_directory(getRoute())) {
+				std::string line{};
+				m_response <<
+ 					"HTTP/1.1 200 OK\r\n"
+  				"Content-Type: text/html\r\n"
+  				"Content-Length: " << std::filesystem::file_size(getRoute()) << "\r\n"
+					//  			"Connection: Close\r\n"
+  				"\r\n"; 
+				while(std::getline(file, line)) {
+  				m_response << line << '\n';	
+  			}
+  			//	 m_response << '\n';
+  			std::cout << "sending code 200\n";
+  		}
+			else {
+		  	generate404();
+			}
+  	}
+  }
 	else {
     generate404();
 	}
 }
-	void requestHandler::generate404() {
-		std::fstream file404{};
-		std::string path404 {"404.html"};
-		file404.open(path404, std::ios::in);
-		m_response << 
-			"HTTP/1.1 404 Not Found\r\n"
-    	"Content-Type: text/html; charset=UTF-8\r\n"
-  		"Content-Length: " << std::filesystem::file_size(path404) << "\r\n"
-  		"\r\n"; 
-		std::string line404 {};
-		while(std::getline(file404, line404)) {
-  		m_response << line404 << '\n';	
-  	}
-  	std::cout << getRoute() << " is not a valid path, sending 404\n";
-	}
+void requestHandler::generate404() {
+	std::fstream file404{};
+	std::string path404 {"404.html"};
+	file404.open(path404, std::ios::in);
+	m_response << 
+		"HTTP/1.1 404 Not Found\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n"
+  	"Content-Length: " << std::filesystem::file_size(path404) << "\r\n"
+  	"\r\n"; 
+	std::string line404 {};
+	while(std::getline(file404, line404)) {
+  	m_response << line404 << '\n';	
+  }
+  std::cout << getRoute() << " is not a valid path, sending 404\n";
+}
