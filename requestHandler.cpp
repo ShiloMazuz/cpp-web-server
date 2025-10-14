49072c86789f45352af94c85e2216f679c56b08a
Shilo <shiloteam@gmail.com>
2025-10-14 20:01:33 +0300

    finished adding parity between requestHandler class and main

--- requestHandler.cpp
+++ requestHandler.cpp
@@ -24,3 +24,91 @@ void requestHandler::getRequestHeader () {
 	setMethodType(method);
 	m_route.setPath(urlPath);
 }
+
+std::string requestHandler::fetchParameter(std::string requestStr, std::string parameter) {
+	std::size_t parameterPosition = requestStr.find(parameter);
+	std::size_t lineEnd = requestStr.find("\r", parameterPosition);
+  return (requestStr.substr(parameterPosition + parameter.size(), lineEnd) );
+}
+
+void requestHandler::methodToResponse() {
+  switch (m_methodType) {
+    case MethodType::GET:
+      generateGetResponse();
+      break;
+    default:
+      generate404();
+  }
+}
+
+void requestHandler::generateGetResponse() {
+	if(getRoute() == "/") {
+		m_response <<
+ 			"HTTP/1.1 200 OK\r\n"
+    	"Content-Type: text/html; charset=UTF-8\r\n\r\n";
+  	std::cout << "sending code 200\n";
+	}
+
+	else if (getRoute() == "/user-agent") {
+		std::string userAgent { fetchParameter(getRequestString(), "User-Agent: ") };
+		m_response <<
+ 			"HTTP/1.1 200 OK\r\n"
+    	"Content-Type: text/html; charset=UTF-8\r\n"
+    	"Content-Length: " << userAgent.size() << "\r\n"
+    	"\r\n" <<
+    	userAgent;
+  	std::cout << "sending code 200\n";
+	}
+	else if(getRoute().substr(0, 6) == "/echo/") {
+		setRoute(getRoute().erase(0, 6));
+		m_response <<
+ 			"HTTP/1.1 200 OK\r\n"
+  		"Content-Type: text/plain\r\n"
+  		"Content-Length: " << getRoute().size() << "\r\n"
+  		"\r\n" << getRoute() << '\n';
+  	std::cout << "sending code 200\n";
+
+	}
+
+	//else if(getRoute().substr(0, 7) == "/files/" && argDirectory) {
+	else if(getRoute().substr(0, 7) == "/files/") {
+		setRoute(getRoute().erase(0, 7));
+		std::fstream file {};
+		file.open(getRoute(), std::ios::in);
+		if(file.is_open() && !std::filesystem::is_directory(getRoute())) {
+			std::string line{};
+			m_response <<
+ 				"HTTP/1.1 200 OK\r\n"
+  			"Content-Type: text/html\r\n"
+  			"Content-Length: " << std::filesystem::file_size(getRoute()) << "\r\n"
+				//  			"Connection: Close\r\n"
+  			"\r\n"; 
+			while(std::getline(file, line)) {
+  			m_response << line << '\n';	
+  		}
+  		//	 m_response << '\n';
+  		std::cout << "sending code 200\n";
+  	}
+		else {
+		  generate404();
+		}
+	}
+	else {
+    generate404();
+	}
+}
+	void requestHandler::generate404() {
+		std::fstream file404{};
+		std::string path404 {"404.html"};
+		file404.open(path404, std::ios::in);
+		m_response << 
+			"HTTP/1.1 404 Not Found\r\n"
+    	"Content-Type: text/html; charset=UTF-8\r\n"
+  		"Content-Length: " << std::filesystem::file_size(path404) << "\r\n"
+  		"\r\n"; 
+		std::string line404 {};
+		while(std::getline(file404, line404)) {
+  		m_response << line404 << '\n';	
+  	}
+  	std::cout << getRoute() << " is not a valid path, sending 404\n";
+	}
--- requestHandler.h
+++ requestHandler.h
@@ -13,19 +13,37 @@ enum class MethodType {
 
 class requestHandler {
 public:
-requestHandler(std::string& requestStr);
+  requestHandler(std::string& requestStr);
 
 private:
-std::stringstream m_requestString {};
-MethodType m_methodType{ MethodType::GET };
-URL m_route { "/" };
+  std::stringstream m_requestString {};
+  MethodType m_methodType{ MethodType::GET };
+  URL m_route { "/" };
+  std::stringstream m_response {};
+
+private:
+  void methodToResponse();
+  void generateGetResponse();
+  std::string fetchParameter(std::string requestStr, std::string parameter);
+	void generate404();
 
 public:
+  //getter functions
   void getRequestHeader ();
   std::string getRoute()          { return m_route.getPath(); };
   std::string getRequestString()  { return m_requestString.str(); };
   MethodType getMethodType()      { return m_methodType; };
+
+  void generateResponseToRequest() {
+    methodToResponse();
+  }
+
+  std::string getResponseStr() {
+    return m_response.str();
+  }
+
   MethodType methodToEnum(std::string method);
+  //setter functions
   void setMethodType(MethodType methodType) { m_methodType = methodType; }
   void setMethodType(std::string& methodType) { m_methodType = methodToEnum(methodType); }
   void setRoute(std::string route) { m_route.setPath(route); };
